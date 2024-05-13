f6f61724a80cb66513b80c9f05accb280312f6fd
RomainJlt <romainjollivet0@gmail.com>
2024-05-13 07:57:24 +0200

    [del all]

--- public/cursor.js
+++
@@ -1,6 +0,0 @@
-// Sélectionnez l'élément HTML sur lequel vous souhaitez appliquer le curseur personnalisé
-const element = document.querySelector('body');
-
-// Modifiez le style du curseur en utilisant l'image personnalisée
-element.style.cursor = 'url(./cursor.png), auto';
-
