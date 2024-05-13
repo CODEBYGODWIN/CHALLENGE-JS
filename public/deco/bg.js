12a9ccdd87c7f9fd6896102864141d1699814042
CODEBYGODWIN <oblasseg18@gmail.com>
2024-05-14 01:23:04 +0200

    [UPDATE] cleaning the code

--- public/deco/bg.js
+++ public/deco/bg.js
@@ -1,76 +1,47 @@
-// Cette fonction est exécutée lorsque le DOM est entièrement chargé
-document.addEventListener("DOMContentLoaded", function() {
-    // Tableau des chemins des images
-    const images = [
-      "../static/img/liasse.webp",
-      "../static/img/sac dollars.png",
-      "../static/img/Gold.png",
-    ];
-  
-    // Index de l'image actuellement affichée
-    let currentImageIndex = 0;
-  
-    // Sélectionne le conteneur d'images avec la classe ".bg"
-    const imageContainer = document.querySelector(".bg");
-  
-    // Crée un élément image et ajoute-lui la classe "bgMove"
-    const imgElement = document.createElement("img");
-    imgElement.classList.add("bgMove");
-  
-    // Ajoute l'élément image au conteneur d'images
-    imageContainer.appendChild(imgElement);
-  
-    // Définit l'identifiant de l'élément image
-    imgElement.id = "bgMove";
-  
-    // Fonction pour changer l'image de fond à intervalles réguliers
-    function changeBackgroundImage() {
-      // Réduit progressivement l'opacité de l'image
-      imgElement.style.opacity = "0";
-      setTimeout(function() {
-        // Change l'image après une courte pause pour la transition
-        imgElement.src = images[currentImageIndex];
-        currentImageIndex = (currentImageIndex + 1) % images.length;
+  document.addEventListener("DOMContentLoaded", function() {
+      const images = [
+        "../static/img/liasse.webp",
+        "../static/img/sac dollars.png",
+        "../static/img/Gold.png",
+      ];
+    
+      let currentImageIndex = 0;
+      const imageContainer = document.querySelector(".bg");
+      const imgElement = document.createElement("img");
+      imgElement.classList.add("bgMove");
+      imageContainer.appendChild(imgElement);
+      imgElement.id = "bgMove";
+
+      function changeBackgroundImage() {
+        imgElement.style.opacity = "0";
+
         setTimeout(function() {
-          // Rétablit l'opacité à 0.2 après avoir changé l'image
-          imgElement.style.opacity = "0.2";
-        }, 50); // Attend un court instant avant de rétablir l'opacité pour permettre à la transition de se terminer
-      }, 5); // Attend 5 ms avant de changer l'image, ce qui donne le temps pour la transition d'opacité de se terminer
-  
-      // Appelle à nouveau la fonction pour le prochain changement d'image après 10 secondes
-      setTimeout(changeBackgroundImage, 10000); // Change toutes les 10 secondes
-    }
-  
-    // Appelle la fonction pour démarrer le diaporama
-    changeBackgroundImage();
+          imgElement.src = images[currentImageIndex];
+          currentImageIndex = (currentImageIndex + 1) % images.length;
+          setTimeout(function() {
+            imgElement.style.opacity = "0.2";
+          }, 50);
+        }, 5);
+        setTimeout(changeBackgroundImage, 10000);
+
+      }
+      changeBackgroundImage();
   });
   
-  // Cette fonction est exécutée lorsque le DOM est entièrement chargé
   document.addEventListener('DOMContentLoaded', function() {
-    // Sélectionne l'élément image avec l'identifiant "bgMove"
     var bg = document.getElementById('bgMove');
     
-    // Fonction pour obtenir une position aléatoire sur la page
     function getRandomPosition() {
-      // Calcule les limites de la fenêtre pour la position maximale
       var maxX = window.innerWidth - bg.clientWidth;
       var maxY = window.innerHeight - bg.clientHeight;
-      
-      // Génère des coordonnées X et Y aléatoires dans ces limites
       var randomX = Math.floor(Math.random() * maxX);
       var randomY = Math.floor(Math.random() * maxY);
-      
       return { x: randomX, y: randomY };
     }
-  
-    // Fonction pour déplacer l'image de fond à une position aléatoire
+
     function movebg() {
-      // Obtiens une nouvelle position aléatoire
       var newPosition = getRandomPosition();
-      // Définit la position de l'image en fonction de la nouvelle position aléatoire
       bg.style.transform = 'translate(' + newPosition.x + 'px, ' + newPosition.y + 'px)';
     }
-  
-    // Définit la fréquence de déplacement de l'image de fond à 2500 ms (2,5 secondes)
     setInterval(movebg, 2500); 
   });
--- public/deco/graphCount.js
+++ public/deco/graphCount.js
@@ -1,85 +1,83 @@
-    
-    const graphCount = (function(){
+const graphCount = (function(){
+    document.addEventListener("DOMContentLoaded", function () {
+        const graphCanvas = document.getElementById("graphCount");
+        const ctx = graphCanvas.getContext("2d");
+        const initialData = {
+            labels: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre"],
+            datasets: [{
+                label: "Dépenses",
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                borderColor: "rgba(255, 99, 132, 1)",
+                borderWidth: 1,
+                data: Array(12).fill(0)
+            }, {
+                label: "Revenus",
+                backgroundColor: "rgba(54, 162, 235, 0.2)",
+                borderColor: "rgba(54, 162, 235, 1)",
+                borderWidth: 1,
+                data: Array(12).fill(0)
+            }]
+        };
 
-        document.addEventListener("DOMContentLoaded", function () {
-            const graphCanvas = document.getElementById("graphCount");
-            const ctx = graphCanvas.getContext("2d");
-            const initialData = {
-                labels: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre"],
+        const myChart = new Chart(ctx, {
+            type: "bar",
+            data: initialData,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        function getMonthFromStringDate(dateString) {
+            const parts = dateString.split('/');
+            return parseInt(parts[1]) - 1;
+        }
+
+        function updateChart() {
+            const userEmail = localStorage.getItem('userEmail');
+            const budgetData = JSON.parse(localStorage.getItem(`budgetData-${userEmail}`));
+            const expenses = budgetData.expenses || [];
+            const incomes = budgetData.incomes || [];
+
+            const newData = {
+                labels: initialData.labels.slice(),
                 datasets: [{
                     label: "Dépenses",
-                    backgroundColor: "rgba(255, 99, 132, 0.2)",
-                    borderColor: "rgba(255, 99, 132, 1)",
-                    borderWidth: 1,
+                    backgroundColor: initialData.datasets[0].backgroundColor,
+                    borderColor: initialData.datasets[0].borderColor,
+                    borderWidth: initialData.datasets[0].borderWidth,
                     data: Array(12).fill(0)
                 }, {
                     label: "Revenus",
-                    backgroundColor: "rgba(54, 162, 235, 0.2)",
-                    borderColor: "rgba(54, 162, 235, 1)",
-                    borderWidth: 1,
+                    backgroundColor: initialData.datasets[1].backgroundColor,
+                    borderColor: initialData.datasets[1].borderColor,
+                    borderWidth: initialData.datasets[1].borderWidth,
                     data: Array(12).fill(0)
                 }]
             };
 
-            const myChart = new Chart(ctx, {
-                type: "bar",
-                data: initialData,
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
+            expenses.forEach(expense => {
+                if (expense.type === 'expense'){
+                    const expenseMonth = getMonthFromStringDate(expense.date);
+                    newData.datasets[0].data[expenseMonth] += parseFloat(expense.amount);
                 }
             });
 
-            function getMonthFromStringDate(dateString) {
-                const parts = dateString.split('/');
-                return parseInt(parts[1]) - 1;
-            }
+            incomes.forEach(income => {
+                if (income.type === 'income'){
+                    const incomeMonth = getMonthFromStringDate(income.date);
+                    newData.datasets[1].data[incomeMonth] += parseFloat(income.amount);
+                }
+            });
 
-            function updateChart() {
-                const userEmail = localStorage.getItem('userEmail');
-                const budgetData = JSON.parse(localStorage.getItem(`budgetData-${userEmail}`));
-                const expenses = budgetData.expenses || [];
-                const incomes = budgetData.incomes || [];
-
-                const newData = {
-                    labels: initialData.labels.slice(),
-                    datasets: [{
-                        label: "Dépenses",
-                        backgroundColor: initialData.datasets[0].backgroundColor,
-                        borderColor: initialData.datasets[0].borderColor,
-                        borderWidth: initialData.datasets[0].borderWidth,
-                        data: Array(12).fill(0)
-                    }, {
-                        label: "Revenus",
-                        backgroundColor: initialData.datasets[1].backgroundColor,
-                        borderColor: initialData.datasets[1].borderColor,
-                        borderWidth: initialData.datasets[1].borderWidth,
-                        data: Array(12).fill(0)
-                    }]
-                };
-
-                expenses.forEach(expense => {
-                    if (expense.type === 'expense'){
-                        const expenseMonth = getMonthFromStringDate(expense.date);
-                        newData.datasets[0].data[expenseMonth] += parseFloat(expense.amount);
-                    }
-                });
-
-                incomes.forEach(income => {
-                    if (income.type === 'income'){
-                        const incomeMonth = getMonthFromStringDate(income.date);
-                        newData.datasets[1].data[incomeMonth] += parseFloat(income.amount);
-                    }
-                });
-
-                myChart.data = newData;
-                myChart.update();
-            }
-            updateChart();
-            document.addEventListener('incomeAdded', updateChart);
-            document.addEventListener('expenseAdded', updateChart);
-        });
-    })();
+            myChart.data = newData;
+            myChart.update();
+        }
+        updateChart();
+        document.addEventListener('incomeAdded', updateChart);
+        document.addEventListener('expenseAdded', updateChart);
+    });
+})();
